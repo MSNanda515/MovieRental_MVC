f3b6707e8c26b437107f13266ec4ce653d871689
Meharpreet Singh Nanda <msnanda515@gmail.com>
2021-06-26 10:29:59 -0600

    Controller for customer

--- Controllers/CustomersController.cs
+++ Controllers/CustomersController.cs
@@ -3,17 +3,34 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-
-// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+using MovieRental.Models;
 
 namespace MovieRental.Controllers
 {
     public class CustomersController : Controller
     {
-        // GET: /<controller>/
+        // GET: customers/
         public IActionResult Index()
         {
-            return View();
+            var customers = GetCustomers();
+            return View(customers);
+        }
+
+        public IActionResult Details(int id)
+        {
+            var customer = GetCustomers().SingleOrDefault(cust => cust.Id == id);
+            if (customer == null)
+                return Content("Page Not found");
+            return View(customer);
+        }
+
+        private IEnumerable<Customer> GetCustomers()
+        {
+            return new List<Customer>
+            {
+                new Customer {Id = 1, Name = "Joy"},
+                new Customer {Id = 2, Name = "Kevin"}
+            };
         }
     }
 }
