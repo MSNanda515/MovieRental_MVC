b9500f66195ce07e61fade5b56d8f439ca2e1e96
Meharpreet Singh Nanda <msnanda515@gmail.com>
2021-07-07 12:28:04 -0600

    Building web api

---
+++ Controllers/Api/CustomersController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MovieRental.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace MovieRental.Views.Customers.Api
+{
+    [Route("api/customers")]
+    public class CustomersController : Controller
+    {
+        private AppDbContext _context;
+
+        public CustomersController()
+        {
+            _context = new AppDbContext();
+        }
+
+
+        // GET: api/values
+        [HttpGet]
+        public IEnumerable<Customer> GetCustomers()
+        {
+            return _context.Customers.ToList();
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
