e54dac95187bb36c2e3cd69f2e3efd05aa5d91de
Meharpreet Singh Nanda <msnanda515@gmail.com>
2021-07-07 12:50:09 -0600

    Built the customer web api

--- Controllers/Api/CustomersController.cs
+++ Controllers/Api/CustomersController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using MovieRental.Models;
+using System.Web;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -20,36 +21,67 @@ namespace MovieRental.Views.Customers.Api
         }
 
 
-        // GET: api/values
+        // GET: api/customers
         [HttpGet]
         public IEnumerable<Customer> GetCustomers()
         {
             return _context.Customers.ToList();
         }
-
-        // GET api/values/5
+         
+        // GET api/customers/1
         [HttpGet("{id}")]
-        public string Get(int id)
+        public Customer GetCustomers(int id)
         {
-            return "value";
+            var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
+
+            if (customer == null)
+                return new Customer();
+
+            return customer;
         }
 
-        // POST api/values
+        // POST api/customers
         [HttpPost]
-        public void Post([FromBody] string value)
+        public Customer CreateCustomer(Customer customer)
         {
+            if (!ModelState.IsValid)
+                return null;
+            _context.Customers.Add(customer);
+            _context.SaveChanges();
+
+            return customer;
         }
 
-        // PUT api/values/5
+        // PUT api/customers/5
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
+        public void UpdateCustomer(int id, Customer customer)
         {
+            if (!ModelState.IsValid)
+                return;
+
+            var customerInDb = _context.Customers.SingleOrDefault(c => c.Id == id);
+
+            if (customerInDb == null)
+                return;
+            customerInDb.Name = customer.Name;
+            customerInDb.Birthdate = customer.Birthdate;
+            customerInDb.IsSubscribedToLetter = customer.IsSubscribedToLetter;
+            customerInDb.MembershipTypeId = customer.MembershipTypeId;
+
+            _context.SaveChanges();
         }
 
-        // DELETE api/values/5
+        // DELETE api/customers/5
         [HttpDelete("{id}")]
-        public void Delete(int id)
+        public void DeleteCustomer(int id)
         {
+            var CustomerInDb = _context.Customers.SingleOrDefault(c => c.Id == id);
+
+            if (CustomerInDb == null)
+                return;
+
+            _context.Customers.Remove(CustomerInDb);
+            _context.SaveChanges();
         }
     }
 }
